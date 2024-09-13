0d18d71ea9ec0643a2b0ff0c8924e06ba3c7befa
Gabriel Fernandes <https://github.com/gabriel-fernands/Maratona_Java>
2024-09-13 18:20:13 -0400

    Aula 03 Operadores Relacionais

--- src/academy/devdojo/maratona/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratona/Aula02TiposPrimitivos.java
@@ -1,4 +1,16 @@
 package academy.devdojo.maratona;
 
+import java.time.LocalDateTime;
+
 public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        String nome = "kirito";
+        String endereco = "Av João das galileia";
+        double salario = 5431.12;
+        String dtaRecebimentoSalario = "13/09/2024";
+        String relatorio = "Eu " + nome + " morando no endereço " + endereco + "confirmo que recebi o salario de " + salario + "na data " + dtaRecebimentoSalario;
+        System.out.println(relatorio);
+
+    }
+
 }
--- src/academy/devdojo/maratona/Aula03Operadores.java
+++ src/academy/devdojo/maratona/Aula03Operadores.java
@@ -1,4 +1,27 @@
 package academy.devdojo.maratona;
 
 public class Aula03Operadores {
+    public static void main(String[] args) {
+        // + - / *
+        int numero01 = 10;
+        int numero02 = 20;
+        double resultado = numero02 / (double) numero01;
+        System.out.println(resultado);
+        // %
+        int resto = 21 % 7;
+        System.out.println(resto);
+        // < >
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 20 < 10;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isVinteIgualDez = 10 == 10;
+        boolean isDezDiferenteDez = 10 != 10;
+
+        System.out.println("isDezIgualVinte " + isDezIgualVinte);
+        System.out.println("isVinteIgualDez " + isVinteIgualDez);
+        System.out.println("isDezMaiorQueVinte " + isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte " + isDezMenorQueVinte);
+        System.out.println("isDezDiferenteDez " + isDezDiferenteDez);
+
+    }
 }
