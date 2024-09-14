932053a912820ef7296e369d3a9300fd238734d2
Gabriel Fernandes <https://github.com/gabriel-fernands/Maratona_Java>
2024-09-14 17:24:08 -0400

    Aula 04 estruturas condicionais operador ternario pt02

---
+++ src/academy/devdojo/maratona/Aula04EstruturasCondicionais02.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratona;
+
+public class Aula04EstruturasCondicionais02 {
+    public static void main(String[] args) {
+        // doar se salario > 5000
+        double salario = 6000;
+        //(cindição) ? verdadeiro : falso
+        String resultado = salario > 5000 ? "eu vou doar 500 pro devdojo":"ainda não tenho condições, mas vou ter!";
+        System.out.println(resultado);
+
+
+    }
+}
