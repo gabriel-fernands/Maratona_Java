11c3ae7f443366bb768a9a03f1e10e8b37b7c1fb
Gabriel Fernandes <https://github.com/gabriel-fernands/Maratona_Java>
2024-09-14 12:06:46 -0400

    Aula 04 estruturas condicionais pt01

--- src/academy/devdojo/maratona/Aula03Operadores.java
+++ src/academy/devdojo/maratona/Aula03Operadores.java
@@ -37,6 +37,25 @@ public class Aula03Operadores {
        boolean isPlaystationCincoCompravel =  valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
         System.out.println("isPlaystationCincoCompravel " + isPlaystationCincoCompravel);
 
+        // = += -= /= %=
+        double bonus = 1800;
+        bonus = bonus + 1000;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.println(bonus);
+        // ++ --
+        int contador = 0;
+        contador = contador + 1;
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        System.out.println(contador);
+        int contador2 = 0;
+        System.out.println(++contador2);
+        System.out.println(contador2);
 
 
     }
---
+++ src/academy/devdojo/maratona/Aula04EstruturasCondicionais.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratona;
+
+public class Aula04EstruturasCondicionais {
+    public static void main(String[] args) {
+        int idade = 20;
+        boolean isAutoricadoComprarBebida = idade >= 18;
+        if (isAutoricadoComprarBebida) {
+            System.out.println(" Autorizado a compra bebida alcólica");
+        }
+        if (!isAutoricadoComprarBebida) {
+            System.out.println("Não Autorizado a comprar bebida alcólica");
+        }
+        boolean c = false;
+        if (c == false){
+            System.out.println("dentro de algo que nunca deve ser feito");
+        }
+    }
+}
