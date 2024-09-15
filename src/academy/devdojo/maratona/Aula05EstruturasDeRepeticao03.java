2040627d91bbc21cdb36778024855866308d8563
Gabriel Fernandes <https://github.com/gabriel-fernands/Maratona_Java>
2024-09-15 12:54:18 -0400

    Aula 05 estruturas de repetição 02 ao 04

---
+++ src/academy/devdojo/maratona/Aula05EstruturasDeRepeticao03.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratona;
+
+public class Aula05EstruturasDeRepeticao02 {
+    public static void main(String[] args) {
+    // imprima todos os números pares de 0 até 1000000
+        for (int i = 0; i < 1000000; i++) {
+            System.out.println(++i);
+        }
+    }
+}
